bf63bde07b427b117819680ec77ab271f552591a
Francisco Javier Cardama Santiago <36961760+CardamaS99@users.noreply.github.com>
2026-06-09 10:13:22 +0200

    Bump version from 0.1.0 to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netqmpi',
-    version='0.1.0',
+    version='0.2.0',
     entry_points={
         'console_scripts': [
             'netqmpi=netqmpi.runtime.cli:main', 
